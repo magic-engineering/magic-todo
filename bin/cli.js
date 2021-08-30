614f10f58b0cff2a4407f647ec64c5de829c1165
neil paul molina <get.neilmolina@gmail.com>
2021-08-30 18:54:00 +0800

    mark as done feature

--- bin/cli.js
+++ bin/cli.js
@@ -15,23 +15,45 @@ const yargsConfiguration = (yargs) => {
 }
 
 const processArgs = async (args) => {
-  const data = await db.JSON();
-  if (args.action == 'add') {
-    const { todos = [] } = data;
-    const nextIndex = todos.length;
-    console.log(`adding ${nextIndex}:${args.actionArg} in todos.`);
-    todos.push(args.actionArg);
-    db.JSON({ todos });
-    await db.sync();
-  } else {
-    console.log('TODOS:');
-    const { todos = [] } = data;
-    todos.forEach((todo, i) => {
-      console.log(`${i}: ${todo}`);
-    });
-    if (!todos.length) {
-      console.log('no todos yet');
-    }
+  switch (args.action) {
+    /**
+     * add a todo item in todos list
+     */
+    case 'add':
+      const data = await db.JSON();
+      const { todos = [] } = data;
+      const nextIndex = todos.length;
+      console.log(`adding ${nextIndex}:${args.actionArg} in todos.`);
+      todos.push(args.actionArg);
+      db.JSON({ todos });
+      break;
+
+    /**
+     * flag a todo item as done
+     */
+    case 'done':
+      let rm_index = +args.actionArg;
+      let data2 = (await db.JSON()) || { todos: []};
+      let todo = data2.todos[rm_index];
+      delete data2.todos[rm_index];
+      console.log('done ${rm_index}:${todo}');
+      db.JSON({ todos });
+      break;
+
+    /**
+     * list all todos
+     */
+    case 'ls':
+    default:
+      const data3 = (await db.JSON()) || {todos: []};
+      console.log('TODOS:');
+      data3.todos.forEach((todo, i) => {
+        console.log(`${i}: ${todo}`);
+      });
+      if (!data3.todos.length) {
+        console.log('no todos yet');
+      }
+      break;
   }
 }
 
